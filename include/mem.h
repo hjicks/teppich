ff7b906c28f2707f0ad7736738495a42a3b23ce8
mkf <mkf@cloud9p.org>
2023-11-17 09:28:35 +0000

    add mem.h

---
+++ include/mem.h
@@ -0,0 +1,13 @@
+#pragma once
+#define MEM_MAX 480
+#define BLOCKSIZE 64
+#define MEM_BEG 0x7E00
+#define MEM_END 0x7FFFF
+
+#define FREE 0
+#define TAKEN 1
+
+uint8 memap[MEM_MAX * 1024 / BLOCKSIZE];
+
+int _malloc(uint16 size);
+void* malloc(uint16 size);
