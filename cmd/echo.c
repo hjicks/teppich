b9d66bc012e6a431a75bf4d9a138cf6c2a8eb6f6
mkf <mkf@cloud9p.org>
2023-11-24 08:45:46 +0000

    cmd/echo: use new printf functions :^)

--- cmd/echo.c
+++ cmd/echo.c
@@ -1,27 +1,28 @@
 #include <u.h>
 #include <libc.h>
 
-#include <vga.h>
 #include <bitmap.h>
 
 void
 echo_main(int argc, char **argv)
 {
 	int i;
-	void (*puts)(char*);
-	
-	puts = vga_puts;
+
 	i = 1;
 	if(argc >= 1 && !strcmp(argv[1], "-b"))
 	{
-		puts = bitputs;
 		i = 2;
+		while(i < argc)
+		{
+			bitputs(argv[i]);
+			i++;
+		}
 	}
 	
 	while(i < argc)
 	{
-		puts(argv[i]);
-		puts(" ");
+		printf("%s", argv[i]);
+		printf(" ");
 		i++;
 	}
 	
