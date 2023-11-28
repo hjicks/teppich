314e6ae39dad35e4d77c9fa6eb99ee2f43a5bf4c
mkf <mkf@cloud9p.org>
2023-11-28 13:38:10 +0000

    add ls into rc.h

--- cmd/rc.h
+++ cmd/rc.h
@@ -5,6 +5,7 @@ int echo_main(int, char**, char *cwd);
 int clear_main(int, char**, char *cwd);
 int check_main(int, char**, char *cwd);
 int cal_main(int, char**, char *cwd);
+int ls_main(int, char**, char *cwd);
 
 static prog_t cmdtab[] =
 {
@@ -12,4 +13,5 @@ static prog_t cmdtab[] =
 	{"clear", clear_main},
 	{"check", check_main},
 	{"cal", cal_main},
+	{"ls", ls_main},
 };
