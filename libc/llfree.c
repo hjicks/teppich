5bb11ec674b1d8ea7693a5f9120280f1102c5796
mkf <mkf@cloud9p.org>
2023-11-28 07:30:41 +0000

    libc/llfree: free all entries in a linked list

---
+++ libc/llfree.c
@@ -0,0 +1,23 @@
+/* 
+	free all entries in a linked list,
+	can, and will be dangrous
+*/
+
+#include <u.h>
+#include <libc.h>
+#include <mem.h>
+
+void
+llfree(ll_t *head)
+{
+	ll_t *t, *p;
+	
+	p = head;
+	t = (ll_t*)head->next;
+	while(t != nil)
+	{
+		free(p);
+		p = t;
+		t = (ll_t*)head->next;
+	}
+}
