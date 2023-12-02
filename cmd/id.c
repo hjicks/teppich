eb0e2965408bc21ef6f53a495f9d6469659c65ec
mkf <mkf@cloud9p.org>
2023-12-02 23:18:07 +0000

    id: update to use new itoa

--- cmd/id.c
+++ cmd/id.c
@@ -13,23 +13,28 @@ usage(void)
 int
 id_main(int argc, char **argv)
 {
+	char *tmp;
 	ll_t *t;
 	user_t *u;
 
 	if(argc != 2)
 		return usage();
 
+	tmp = malloc(2 << 8);
 	u = nil;
 	t = users;
 	while(t != nil)
 	{
 		u = (user_t*)t->val;
 		/* i wanted to add a iflag, but let it do both for now */
-		if(!strcmp(u->name, argv[1]) || !strcmp(itoa(u->id, 10), argv[1]))
+		if(!strcmp(u->name, argv[1]) || !strcmp(itoa(u->id, tmp, 10), argv[1]))
 				break;
 		u = nil;
 		t = (ll_t*)t->next;
 	}
+
+	free(tmp);
+
 	if(u == nil)
 	{
 		printf("No such user or id\n");
