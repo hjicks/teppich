8fe978f00633ce7bdf4c0413e72b5a78ddca7bec
mkf <mkf@cloud9p.org>
2023-12-03 10:26:05 +0000

    fstab: add envfs

--- fs/fstab.h
+++ fs/fstab.h
@@ -1,11 +1,13 @@
 #pragma once
 #include <vfs.h>
 
-fs_t* cmdfs_init(void);
-fs_t* rootfs_init(void);
+extern fs_t* cmdfs_init(void);
+extern fs_t* rootfs_init(void);
+extern fs_t* envfs_init(void);
 
 static mnt_t fstab[] =
 {
 	{"/", rootfs_init},
 	{"/cmd/", cmdfs_init},
+	{"/env/", envfs_init},
 };
